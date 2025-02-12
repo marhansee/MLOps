0051efd0578d73b0cf00739d1425d851364269c6
Mardaaa <martin_tp@live.dk>
2025-02-12 11:43:20 +0100

    Fixed bug regarding imread-function from opencv

--- src/new_inference.py
+++ src/new_inference.py
@@ -20,12 +20,15 @@ def load_model(model_path, device):
     model.eval()
     return model
 
-def preprocess_image(image_path, device):
+def load_image(image_path):
     """Loads and preprocesses an image for model inference."""
     image = cv2.imread(image_path)
     if image is None:
         raise ValueError("Failed to load the image. Check the file path.")
+    return image
 
+
+def preprocess_image(image, device):
     image = image / 255.0
     image = cv2.resize(image, (275, 275))
     image = (image - np.array([0.485, 0.456, 0.406])) / np.array([0.229, 0.224, 0.225])
@@ -53,8 +56,9 @@ def main():
     classes = [i for i in range(1, 197)]
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = load_model('best_model.pth', device)
-    image_tensor = preprocess_image(image_path, device)
+    model = load_model('model_weights/best_model.pth', device)
+    image = load_image('inference_images/00076.jpg')
+    image_tensor = preprocess_image(image, device)
     predicted_class = classes[predict(model, image_tensor)]
 
     print(f"Predicted Model: {get_class_name(predicted_class)}")
