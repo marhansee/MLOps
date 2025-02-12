da1cf4a3812f741f54fe4b2e52140430f8867059
Mardaaa <martin_tp@live.dk>
2025-02-12 11:40:01 +0100

    moved inference images into a folder to avoid 'file does not exist' error in testing

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -6,9 +6,7 @@ import cv2
 import os
 from unittest.mock import patch, MagicMock
 from src.new_inference import load_model, preprocess_image, predict, get_class_name
-from model import CustomResNet
-
-
+#from model import CustomResNet
 
 
 @pytest.fixture
@@ -23,6 +21,26 @@ def dummy_model():
     model.return_value = torch.tensor([[0.1, 0.9]])
     return model
 
-def test_modelweights_exist():
-    model_path = "best_model.pth"
-    assert os.path.exists(model_path) == True
+@pytest.fixture
+def device():
+    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+@pytest.fixture
+def dummy_image():
+    tensor = torch.randn(1, 3, 300, 300)
+    return tensor
+
+def test_load_model(device):
+    """Test model loading with an invalid path."""
+    with pytest.raises(FileNotFoundError):
+        load_model("best_model.pth", device)
+
+# Create dummy image
+def test_preprocess_image(dummy_image, device):
+    """Test that image is resized to 275x275"""
+    image = preprocess_image(dummy_image, device)
+    assert image.shape == (1, 3, 275, 275), "Image should be resized!"
+
+ 
+
+
